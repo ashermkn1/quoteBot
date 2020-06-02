39860e79a3980e322fab4b3d9b4fd4b5fd9965c7
Asher Kornfeld <ashkornfeld@gmail.com>
2020-06-02 16:16:51 +0000

    added remove command

--- main.py
+++ main.py
@@ -20,16 +20,22 @@ async def on_ready():
 
 
 @bot.command(name='qadd', help="adds a specific message with given message id to a user's list of quotes")
-async def add_quote(ctx: commands.Context, message_id: int):
+async def add_quote(ctx: commands.Context, message_id: int=None):
+    if not message_id:
+      await ctx.send('Please provide an id of the message you want to quote')
+      return
     quote = await ctx.channel.fetch_message(message_id)
-    # quote = quote.content
-    db.execute('INSERT into quotes (user_id, quote, time) VALUES (?, ?, ?)', (quote.author.id, quote.content.strip('```'), datetime.now(tz=timezone(-timedelta(hours=4)))))
+    time = quote.created_at.replace(tzinfo=timezone(-timedelta(hours=4)))
+    db.execute('INSERT into quotes (user_id, quote, time) VALUES (?, ?, ?)', (quote.author.id, quote.content.strip('```'), time))))
     conn.commit()
     await ctx.send("Done!")
 
 
 @bot.command(name='qget', help="gets a random message from given user that has been added using !qadd")
-async def get_quote(ctx: commands.Context, person: User):
+async def get_quote(ctx: commands.Context, person: User=None):
+    if not person:
+       await ctx.send('Please mention a user to get a random quote from')
+       return
     db.execute("SELECT quote, time FROM quotes WHERE user_id=?", (int(person.id),))
     quotes = db.fetchall()
     randQuote = random.choice(quotes)
@@ -42,5 +48,14 @@ async def get_quote(ctx: commands.Context, person: User):
         ```
         ''')
 
-
+@bot.command(name='qremove', help="removes a given message_id from the database")
+async def remove_quote(ctx: commands.Context, message_id: int=None):
+    if not message_id:
+      await ctx.send('Please provide the id of a message you want to remove')
+      return
+    quote = await ctx.channel.fetch_message(message_id)
+    db.execute('DELETE * FROM quotes WHERE quote=?', (quote.content,))
+    conn.commit()
+    await ctx.send('Done!')
+     
 bot.run(TOKEN)
